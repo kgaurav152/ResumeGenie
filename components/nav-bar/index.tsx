3c5dfb597e87f9b5c5a9e0b98789d4a5c76f077a
kgaurav152 <kgaurav152@gmail.com>
2025-02-25 16:22:28 +0530

    added themeChanger to the nav-bar for the public user also

--- components/nav-bar/index.tsx
+++ components/nav-bar/index.tsx
@@ -1,14 +1,23 @@
+"use client";
 import React from "react";
-import { ExternalLink } from "lucide-react";
+import { ChevronDown, Loader, Moon, Sun } from "lucide-react";
 import {
   LoginLink,
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 
 const NavBar = () => {
+  const { setTheme } = useTheme();
   return (
     <div className="shadow-sm w-full sticky top-0  bg-white dark:bg-gray-900 z-[9999]">
       {/* <div className="w-full flex items-center justify-center h-auto bg-black">
@@ -57,6 +66,26 @@ const NavBar = () => {
           </div>
         </div>
         <div className="flex items-center gap-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <LoginLink>
             <Button variant="outline">Sign In</Button>
           </LoginLink>
